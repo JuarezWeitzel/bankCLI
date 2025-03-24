77d1f70d39bda2c8a5fa70e43a4826038d2f1d22
JuarezWeitzel <juarez.zbw@gmail.com>
2025-03-24 16:31:56 -0300

    feat: adiciona buildAccount com criação e validação de contas.

--- index.js
+++ index.js
@@ -9,47 +9,92 @@ const { exit } = require("process");
 operation();
 
 function operation() {
-    inquirer.default
-        .prompt([
-            {
-                type: "list",
-                name: "action",
-                message: "Qual operação deseja realizar?",
-                choices: [
-                    "Criar Conta",
-                    "Consultar Saldo",
-                    "Depositar",
-                    "Sacar",
-                    "Sair"
-                ]
-            }
-        ]).then((answer) => {
+  inquirer.default
+    .prompt([
+      {
+        type: "list",
+        name: "action",
+        message: "Qual operação deseja realizar?",
+        choices: [
+          "Criar Conta",
+          "Consultar Saldo",
+          "Depositar",
+          "Sacar",
+          "Sair",
+        ],
+      },
+    ])
+    .then((answer) => {
+      const action = answer["action"];
 
-            const action = answer["action"];
-
-            if (action === "Criar Conta") {
-                createAccount();
-            } else if (action === "Consultar Saldo") {
-                getAccountBalance();
-            } else if (action === "Depositar") {
-                deposit();
-            } else if (action === "Sacar") {
-                withdraw();
-            } else if (action === "Sair") {
-                leaveBank();
-            }
-
-        }).catch((err) => {
-            console.log(err);
-        })
+      if (action === "Criar Conta") {
+        createAccount();
+      } else if (action === "Consultar Saldo") {
+        getAccountBalance();
+      } else if (action === "Depositar") {
+        deposit();
+      } else if (action === "Sacar") {
+        withdraw();
+      } else if (action === "Sair") {
+        leaveBank();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 function createAccount() {
-    console.log(chalk.bgGreen.black("Obrigado por escolher nosso Banco!"));
-    console.log(chalk.green("Parabéns, sua conta foi criada!"));
-    operation();
+  console.log(chalk.bgGreen.black("Obrigado por escolher nosso Banco!"));
+  console.log(chalk.green("Parabéns, sua conta foi criada!"));
+  buildAccount();
+}
+
+function buildAccount() {
+  inquirer.default
+    .prompt([
+      {
+        name: "accountName",
+        message: "Qual nome deseja dar para sua conta?",
+      },
+    ])
+    .then((answer) => {
+      const accountName = answer["accountName"];
+
+      if (accountName === "" || !isNaN(Number(accountName))) {
+        console.log(chalk.bgRed.black("Nome da conta inválido!"));
+        return buildAccount();
+      }
+
+      if (!fs.existsSync("accounts")) {
+        fs.mkdirSync("accounts");
+      }
+
+      if (fs.existsSync(`accounts/${accountName}.json`)) {
+        console.log(
+          chalk.bgRed.black("Esta conta ja existe, escolha outro nome!")
+        );
+        buildAccount();
+      } else {
+        fs.writeFileSync(
+          `accounts/${accountName}.json`,
+          '{"balance": 0}',
+          function (err) {
+            console.log(err);
+          }
+        );
+
+        console.log(
+          chalk.green("Parabéns, sua conta foi criada! Conta: " + accountName)
+        );
+        operation();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 function leaveBank() {
-    console.log(chalk.bgBlue.black("Obrigado por usar o BankCLI!"));
+  console.log(chalk.bgBlue.black("Obrigado por usar o BankCLI!"));
 }

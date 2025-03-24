2d668c643b4114d6d731a99793098e9723118ec6
JuarezWeitzel <juarez.zbw@gmail.com>
2025-03-24 16:06:08 -0300

    feat: implementa menu principal com a função operation()

--- index.js
+++ index.js
@@ -5,3 +5,42 @@ const chalk = require("chalk");
 // importando modulos internos
 const fs = require("fs");
 const { exit } = require("process");
+const { create } = require("domain");
+
+operation();
+
+function operation() {
+    inquirer.default
+        .prompt([
+            {
+                type: "list",
+                name: "action",
+                message: "Qual operação deseja realizar?",
+                choices: [
+                    "Criar Conta",
+                    "Consultar Saldo",
+                    "Depositar",
+                    "Sacar",
+                    "Sair"
+                ]
+            }
+        ]).then((answer) => {
+
+            const action = answer["action"];
+
+            if (action === "Criar Conta") {
+                createAccount();
+            } else if (action === "Consultar Saldo") {
+                getAccountBalance();
+            } else if (action === "Depositar") {
+                deposit();
+            } else if (action === "Sacar") {
+                withdraw();
+            } else if (action === "Sair") {
+                exit();
+            }
+
+        }).catch((err) => {
+            console.log(err);
+        })
+}

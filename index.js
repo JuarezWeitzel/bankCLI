7bf60010fce924847763e02a2378152ae5c36bc0
JuarezWeitzel <juarez.zbw@gmail.com>
2025-03-24 15:39:58 -0300

    feat: adiciona importação de módulos iniciais no index.js - modulos externos e importando modulos internos.

---
+++ index.js
@@ -0,0 +1,7 @@
+// importando modulos externos
+const inquirer = require("inquirer");
+const chalk = require("chalk");
+
+// importando modulos internos
+const fs = require("fs");
+const { exit } = require("process");
